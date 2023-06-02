dbf2f7518b2605a08de2671509899f915ba9f038
Hamza <43032584+ritopsis@users.noreply.github.com>
2023-06-02 20:18:11 +0200

    API-Testing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+const http = require("https");
 
 // Parse urlencoded bodies
 app.use(bodyParser.json());
@@ -9,7 +10,33 @@ app.use(bodyParser.json());
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, "files")));
 
+app.get("/movie", function(req,res){ //kleine Probe mit https://rapidapi.com/SAdrian/api/moviesdatabase/
+    
+    const options = {
+        host: 'moviesdatabase.p.rapidapi.com',
+        path: '/titles/episode/tt12785836',
+        headers: {
+          'X-RapidAPI-Key': '820810d92fmsh47306384f1838ccp1ad0a6jsnb891ca3b29ec',
+          'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+      };
+http.get(options, (response) => {
 
+    let data = "";
+    response.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    response.on('end', () => {
+    console.log(data);
+    });
+
+    response.on('error', (error) => {
+        console.log(error);
+    });
+});
+
+})
 
 app.listen(3000);
 
