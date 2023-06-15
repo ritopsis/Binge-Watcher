77c3f5bd6ec944be2589d7607a11f6864a39602f
Hamza <43032584+ritopsis@users.noreply.github.com>
2023-06-15 15:07:05 +0200

    small fixes

--- server/files/index.js
+++ server/files/index.js
@@ -1,42 +1,4 @@
 window.onload = function () {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      const result = JSON.parse(xhr.responseText);
-      result.forEach((element) => {
-        addmovie(element);
-      });
-      //console.log(result);
-    } else {
-      document
-        .querySelector("body")
-        .append(
-          `Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`
-        );
-    }
-  };
-  xhr.open("GET", "movie");
-  xhr.send();
-
-  const xhrShows = new XMLHttpRequest();
-  xhrShows.onload = function () {
-    if (xhrShows.status === 200) {
-      const showsResult = JSON.parse(xhrShows.responseText);
-      showsResult.forEach((element) => {
-        addShow(element);
-      });
-      //console.log(showsResult);
-    } else {
-      document
-        .querySelector("body")
-        .append(
-          `Show Daten konnten nicht geladen werden, Status ${xhrShows.status} - ${xhrShows.statusText}`
-        );
-    }
-  };
-  xhrShows.open("GET", "series");
-  xhrShows.send();
-
   const xhrlogin = new XMLHttpRequest();
   const menuItems = document.getElementById("menuItems");
   xhrlogin.onload = function () {
@@ -63,12 +25,47 @@ window.onload = function () {
       menuItems.appendChild(loginLink);
     }
   };
-
   xhrlogin.open("GET", "loggedin");
   xhrlogin.send();
+
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const result = JSON.parse(xhr.responseText);
+      result.forEach((element) => {
+        addarticle(element, ".topmovies");
+      });
+    } else {
+      document
+        .querySelector("body")
+        .append(
+          `Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`
+        );
+    }
+  };
+  xhr.open("GET", "movie", true);
+  xhr.send();
+
+  const xhrShows = new XMLHttpRequest();
+  xhrShows.onload = function () {
+    if (xhrShows.status === 200) {
+      const showsResult = JSON.parse(xhrShows.responseText);
+      showsResult.forEach((element) => {
+        addarticle(element, ".topseries");
+      });
+    } else {
+      document
+        .querySelector("body")
+        .append(
+          `Show Daten konnten nicht geladen werden, Status ${xhrShows.status} - ${xhrShows.statusText}`
+        );
+    }
+  };
+  xhrShows.open("GET", "series", true);
+  xhrShows.send();
 };
 
-function addmovie(movie) {
+function addarticle(movie, documentelement) {
   const articleElement = document.createElement("article");
   articleElement.id = movie.id;
   // Create the link element
@@ -82,7 +79,6 @@ function addmovie(movie) {
     imageElement.src = movie.primaryImage.url;
     imageElement.alt = "Movie Image";
   } else {
-    //console.log("help");
     imageElement.src =
       "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fb3ef66312333.5691dd2253378.jpg";
     imageElement.loading = "lazy";
@@ -102,45 +98,6 @@ function addmovie(movie) {
   articleElement.appendChild(linkElement);
 
   // Add the article element to the document
-  const topMoviesElement = document.querySelector(".topmovies");
+  const topMoviesElement = document.querySelector(documentelement);
   topMoviesElement.appendChild(articleElement);
-}
-
-function addShow(show) {
-  const articleElement = document.createElement("article");
-  articleElement.id = show.id;
-
-  // Create the link element
-  const linkElement = document.createElement("a");
-  linkElement.href = "/details.html?id=" + show.id;
-
-  // Create the image element
-  const imageElement = document.createElement("img");
-  imageElement.classList.add("articleimage");
-  if (show.primaryImage && show.primaryImage.url) {
-    imageElement.src = show.primaryImage.url;
-    imageElement.alt = "Show Image";
-  } else {
-    //console.log("help");
-    imageElement.src =
-      "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fb3ef66312333.5691dd2253378.jpg";
-    imageElement.loading = "lazy";
-    imageElement.alt = "Alternative Image";
-  }
-  //console.log(imageElement.src);
-
-  // Create the heading element
-  const headingElement = document.createElement("h1");
-  headingElement.textContent = show.titleText.text;
-
-  // Append the image and heading elements to the link element
-  linkElement.appendChild(imageElement);
-  linkElement.appendChild(headingElement);
-
-  // Append the link element to the article element
-  articleElement.appendChild(linkElement);
-
-  // Add the article element to the document
-  const topShowsElement = document.querySelector(".topseries");
-  topShowsElement.appendChild(articleElement);
 }
